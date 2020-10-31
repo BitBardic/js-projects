7d36e0ce62746cf968a05aea121e76a41f7421ac
Nasir Iqbal <nasirpucit@hotmail.com>
2020-10-31 15:00:52 +0000

    Added paddle movment

--- BREAKOUT-GAME/script.js
+++ BREAKOUT-GAME/script.js
@@ -6,8 +6,8 @@ const ctx = canvas.getContext('2d');
 
 let score = 0;
 
-const brickRowCount = 9;
-const brickColumnCount = 5;
+const brickColumnCount = 9;
+const brickRowCount = 5;
 
 // Create ball props
 const ball = {
@@ -41,15 +41,15 @@ const brickInfo = {
   
   // Create bricks
   const bricks = [];
-  for (let i = 0; i < brickRowCount; i++) {
+  for (let i = 0; i < brickColumnCount; i++) {
     bricks[i] = [];
-    for (let j = 0; j < brickColumnCount; j++) {
+    for (let j = 0; j < brickRowCount; j++) {
       const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
       const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
       bricks[i][j] = { x, y, ...brickInfo };
     }
   }
-
+console.log(bricks);
 // Draw ball on canvas
 function drawBall() {
     ctx.beginPath();
@@ -68,12 +68,28 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+// Move paddle on canvas
+function movePaddle() {
+  paddle.x += paddle.dx;
+
+  // Wall detection
+  if(paddle.x + paddle.w > canvas.width) {
+    paddle.x = canvas.width - paddle.w;
+  }
+
+  if(paddle.x < 0) {
+    paddle.x = 0;
+  }
+}
+
 // Draw everything
 function draw() {
-    drawBall();
-    drawPaddle();
-    drawScore();
-    drawBricks();
+  // clear canvas
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  drawBall();
+  drawPaddle();
+  drawScore();
+  drawBricks();
 }
 
 // Draw score on canvas
@@ -95,7 +111,38 @@ function drawBricks() {
     });
   }
 
-draw();
+// Update canvas drawing and animation
+function update() {
+
+  movePaddle();
+
+  // Draw everything
+  draw();
+
+  requestAnimationFrame(update);
+}
+
+update();
+
+// Keydown event
+function keyDown(e) {
+  if(e.key === 'Right' || e.key === 'ArrowRight') {
+    paddle.dx = paddle.speed;
+  } else if(e.key === 'Left' || e.key === 'ArrowLeft') {
+    paddle.dx = -paddle.speed
+  }
+}
+
+// Keyup event
+function keyUp(e) {
+  if(e.key === 'Right' || e.key === 'ArrowRight' || e.key === 'Left' || e.key === 'ArrowLeft') {
+    paddle.dx = 0;
+  }
+}
+
+// Keyboard even handlers
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
 
 // Rules and close event handlers
 rulesBtn.addEventListener('click', () => rules.classList.add('show'));
